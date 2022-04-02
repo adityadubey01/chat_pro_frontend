8b6329bac87e41d28e9ea13fa614d910c81ca74a
Aditya Dubey <adityadubey412@gmail.com>
2022-04-02 23:30:43 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+//import ReactDOM from 'react-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; 
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom'
 
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//ChakraProvider();
+BrowserRouter();
+
+
 
 
